4e5b102c6165a7812666a44a67c366b6a5fe16f6
Franz Mathauser <franz.mathauser@googlemail.com>
2011-11-01 22:48:58 +0100

    menü links für audio upload hinzugefügt

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,17 +1,18 @@
 class PlayerController < ApplicationController
   def index
      path=""
-     @useraudio = UserAudio.find(params[:id])
+     @useraudio = UserAudio.where(:user_id=>current_user, :id=>params[:id]).first
+
      @audio = @useraudio.audio
      @audio_path = @audio.id.to_s+".mp3"
      @audioimage_path = @audio.id.to_s+".png"
   end
 
   def list
-    @audios = Array.new
+    @useraudios = Array.new
 
     current_user.user_audios.each do |useraudios|
-      @audios << useraudios.audio
+      @useraudios << useraudios
     end
 
   end

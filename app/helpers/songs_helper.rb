68606574128debbeb8a0752927bbe59ee815850a
Frank <frank.waininger@gmx.de>
2011-11-03 16:15:26 +0100

    Player nach oben verschoben (views/layouts/_player.html.erb)

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,7 +3,7 @@ module SongsHelper
   # Return a wave image
   def audioimage
     base_title = "Ruby on Rails Tutorial Sample App"
-    if @audio.imageprocessed == false
+    if @audio.nil? || @audio.imageprocessed == false
       asset_path "std_audioimage.png"
     else
       asset_path @audioimage_path
